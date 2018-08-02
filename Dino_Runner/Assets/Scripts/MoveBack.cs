6cd098253e387f0b39c8a0a45e0d0ebdba03208f
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-08-02 18:32:32 -0400

    Prototype Done

---
+++ Dino_Runner/Assets/Scripts/MoveBack.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBack : MonoBehaviour {
+
+	private Vector3 pos;
+	public float mSpeed = 10.0f;
+	public Transform orig;
+
+
+	void FixedUpdate() {
+		pos = new Vector3(transform.position.x, transform.position.y, transform.position.z + mSpeed * Time.deltaTime);
+		transform.position = pos;
+	}
+
+	void OnTriggerEnter(Collider col) {
+		if(col.gameObject.tag == "Death") {
+			gameObject.transform.position = orig.transform.position;
+		}
+
+	}
+}
