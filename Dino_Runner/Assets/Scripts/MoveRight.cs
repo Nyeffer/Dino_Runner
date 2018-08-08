ff48530b60623d982e5f77bee55465a03fc805a1
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-08-08 18:22:12 -0400

    Controls and GUI IN

---
+++ Dino_Runner/Assets/Scripts/MoveRight.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class MoveRight : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+	private bool isActive = false;
+	public float movementCap = 10.0f;
+	private float moveCounter = 0.0f;
+	private Vector3 pos;
+	public GameObject player;
+	
+	// Update is called once per frame
+	void Update () {
+		pos = player.transform.position;
+		if(isActive) {
+			if(moveCounter < movementCap) {
+				pos.x -= Time.deltaTime;
+				moveCounter += Time.deltaTime;
+			}
+			player.transform.position = pos;
+		}
+	}
+
+	public virtual void OnPointerDown(PointerEventData ped) {
+		SetisActive(true);
+	}
+
+	public virtual void OnPointerUp(PointerEventData ped) {
+		SetisActive(false);
+		
+	}
+
+	public void SetisActive(bool activate) {
+		isActive = activate;		
+	}
+}
