6f8e6d8f47106ce9b359bf5c97b96d524704a7cc
Jeffherson Vega <jeffhersonlim@gmail.com>
2018-08-15 21:15:13 -0400

    Spawns are In

---
+++ Dino_Runner/Assets/Scripts/Spawn.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+	public Transform[] spawns;
+	public GameObject[] obstacles;
+	private Vector3 desiredPosition;
+	private GameObject desiredObstacle;
+	public float SecPerSpawn;
+	private float spawnCounter = 0.0f;
+
+	void Update() {
+		if(spawnCounter >= SecPerSpawn) {
+			Instantiate(PickObstacle(), PickPosition(), Quaternion.identity);
+			spawnCounter = 0;
+		} else {
+			spawnCounter += Time.deltaTime;
+		}
+	}
+
+	GameObject PickObstacle() {
+		int rand = Random.Range(0, obstacles.Length + 1);
+		switch(rand) {
+			case 0:
+				desiredObstacle = obstacles[0];
+			break;
+			case 1:
+				desiredObstacle = obstacles[1];
+			break;
+			default:
+			break;
+		}
+		return desiredObstacle;
+	}
+
+	Vector3 PickPosition() {
+		int rand = Random.Range(0, obstacles.Length + 1);
+		switch(rand) {
+			case 0:
+				desiredPosition = spawns[0].position;
+			break;
+			case 1:
+				desiredPosition = spawns[1].position;
+			break;
+			case 2:
+				desiredPosition = spawns[2].position;
+			break;
+			default:
+			break;
+		}
+		return desiredPosition;
+	}
+
+
+
+}
